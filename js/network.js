22ea09bb16ec32c27c7a8bdc5a57bba8f6c5093c
vquynh <ng.viet.quynh@gmail.com>
2022-05-22 19:10:17 +0200

    Reduced number of synapses

--- js/network.js
+++ js/network.js
@@ -1,10 +1,10 @@
 // create an array with nodes
 var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" }
+    { id: 1, label: "Neuron 1" },
+    { id: 2, label: "Neuron 2" },
+    { id: 3, label: "Neuron 3" },
+    { id: 4, label: "Neuron 4" },
+    { id: 5, label: "Neuron 5" }
 ]);
 
 // create an array with edges
